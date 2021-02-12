cba585b59fabd71e2cf5aeee54042b828e8ac9b6
Gabo <gabrielbacar@gmail.com>
2021-02-12 00:38:29 -0500

    Created functioning Carousel component

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -2,23 +2,29 @@ import React from 'react';
 import Carousel from 'react-material-ui-carousel'
 import {Paper, Button} from '@material-ui/core'
 
+var style = {
+    maxHeight: '350px',
+    textAlign: 'center',
+    width: '350px'
+    
+}
+
 export default function ImageCarousel(props)
 {
-    var items = [
-        {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
-
+    console.log(props.images)
     return (
-        <Carousel>
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
+        <Carousel fullHeightHover={false} 
+        style={{backgroundColor: 'black'}} 
+        navButtonsAlwaysInvisible={props.images.length === 1} 
+        indicators={props.images.length !== 1}
+        autoPlay={false}
+        >
+            {                
+                props.images.map( (img, i) => 
+                    <div style={{width: '100%', display: 'flex', justifyContent:'center'}}> 
+                    <img key={i} src={img} style={style}/>
+                    </div> 
+                )
             }
         </Carousel>
     )
--- src/routes/AnimalInfo.js
+++ src/routes/AnimalInfo.js
@@ -10,16 +10,43 @@ import Box from '@material-ui/core/Box';
 import Header from '../components/Header.js';
 import PerdidoEncontradoBanner from '../components/PerdidoEncontradoBanner.js';
 import ImageCarousel from '../components/ImageCarousel.js';
+import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
-      backgroundColor: 'white' 
+      backgroundColor: 'white',
+      color: '#777777',
     },
+    grid:{
+        width: '100%'
+    },
+    carouselWrapper:{
+        padding: '20px 0'
+    }
 
-  }));
+}));
 
+function formatImages(animalData){
+    console.log(animalData)
+    var images = []
+    for (var i = 1; i < 6; i++){
+        var attribute = 'imagen_' + i
+        if (animalData[attribute]){
+            images.push(animalData[attribute])
+        }
+    }
+    return images;
+}
+
+function formatBulletInfo(title, info){
+    return(
+        <Box display='flex'>
+            <Typography variant="subtitle1"> Nombre</Typography>
+        </Box>
+    )
+}
 
 export default function AnimalInfo(props){
     let { id } = useParams();
@@ -50,11 +77,28 @@ export default function AnimalInfo(props){
         <div className={classes.root}>
             <Header />
             
-            <Grid container>
-            <PerdidoEncontradoBanner/>
-            <Grid item xs={12}>
-                <ImageCarousel />
-            </Grid>
+            <Grid container direction="row" justify="center" alignItems="center" className={classes.grid}>
+                <Grid item xs={12}>
+                    <PerdidoEncontradoBanner/>
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <Box display='flex' justifyContent="center" alignItems="center" width='100%'>
+                        <Box width='350px' className={classes.carouselWrapper}>
+                            <ImageCarousel images={formatImages(animalData)}/>
+                        </Box>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                <Box display='flex'>
+                    <Typography variant="subtitle1" style={{fontWeight: 100, marginRight: '3px'}}> Nombre: </Typography>
+                    <Typography variant="subtitle1" style={{fontWeight: 600}}> Ruby </Typography>
+                </Box>
+                
+                </Grid>
+
+                
 
             </Grid>
       </div>
